fd742eb1e522fe2ff0cc5fbfa9be60e0ca2c42a2
Vesa A. Susuri <108492136+vesasusuri@users.noreply.github.com>
2022-12-17 23:50:53 +0100

    ourTeam

--- src/components/pages/ourTeam/banner.js
+++ src/components/pages/ourTeam/banner.js
@@ -2,22 +2,9 @@ import "./styles/bannerTeam.scss";
 function BannerOurTeam() {
   return (
     <>
-          <p className="ourteamTitle">Our Team</p>
-        <div className="lineOnOurtTeam">
-        </div>
-      <div className="bannerOurTeam-background" alt="banner">
-      </div>
-
-      <div className="BannerText">
-        <div>
-          {/* {Data.map((props) => {
-            return (
-              <div className="">
-              </div>
-            );
-          })} */}
-        </div>
-      </div>
+      <center><div className="lineOnOurtTeam"></div></center>
+      <p className="ourteamTitle">Our Team</p>
+      <div className="bannerOurTeam-background" alt="banner"></div>
     </>
   );
 }
